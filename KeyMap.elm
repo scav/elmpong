92cdd84ac51b7575e4cbd6954ac8cf5c12705192
Dag Østgulen Heradstveit <dagherad@gmail.com>
2017-07-12 01:00:28 +0200

    Added pattern matching for keys

---
+++ KeyMap.elm
@@ -0,0 +1,26 @@
+{--
+    The simple module containing a very rudamentary keymap
+    that can be used to control the game.
+--}
+module KeyMap exposing (..)
+
+import Keyboard
+-- Represent keys as types to faclilitate pattern matching
+
+-- type alias Key = { keyCode : Keyboard.KeyCode }
+
+type Key
+    = Up
+    | Down
+    | Pause
+    | Undefined
+
+-- Matches a KeyCode to the Key type
+keytype : Int -> Key
+keytype keyCode = 
+    case keyCode of
+        40 -> Up
+        38 -> Down
+        80 -> Pause
+        _ -> Undefined
+
